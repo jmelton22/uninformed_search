49fefb3f47b4a5aa77cc41d5065b15bad202e65c
jmelton22 <jmelton22@gmail.com>
2019-09-10 15:47:59 -0400

    Added initial skeleton for breadth-first uninformed search

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from node import Node
 from grid import read_grid, output_grid
 
 
+def uninformed_search(grid, start, goal):
+    current = Node(start, '')
+    unexplored = Queue()
+    visited = []
+
+    while (current.value != goal) and unexplored.empty():
+        current = unexplored.get()
+        visited.append(current)
+
+        if current == goal:
+            pass
+        else:
+            # Expand node
+            pass
+
+
 def main():
     grid = read_grid('grid.txt')
     start = [1, 1]

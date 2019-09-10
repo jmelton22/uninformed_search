525c1374416d3e95fd06afd0ab9764587daeb784
jmelton22 <jmelton22@gmail.com>
2019-09-10 15:38:10 -0400

    Added Node class and get_neighbors function

--- node.py
+++ node.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+
+class Node:
+    def __init__(self, value, parent):
+        self.value = value
+        self.parent = parent
+
+    def get_neighbors(self, grid):
+        up = [self.value[0] - 1, self.value[1]]
+        right = [self.value[0], self.value[1] + 1]
+        down = [self.value[0] + 1, self.value[1]]
+        left = [self.value[0] - 1, self.value[1]]
+
+        neighbors = []
+
+        if grid[up[0]][up[1]] == 0:
+            neighbors.append(up)
+        if grid[right[0]][right[1]] == 0:
+            neighbors.append(right)
+        if grid[down[0]][down[1]] == 0:
+            neighbors.append(down)
+        if grid[left[0]][left[1]] == 0:
+            neighbors.append(left)
+
+        return neighbors

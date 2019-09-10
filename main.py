d691b14c7977ab845d3a97e045b9421314e83c98
jmelton22 <jmelton22@gmail.com>
2019-09-10 15:40:31 -0400

    Added main method

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+from queue import Queue
+from node import Node
+from grid import read_grid, output_grid
+
+
+def main():
+    grid = read_grid('grid.txt')
+    start = [1, 1]
+    end = [5, 6]
+
+    # Print grid: add space between columns and newline between rows
+    print('\n'.join(' '.join([str(col) for col in row]) for row in grid))
+
+
+if __name__ == '__main__':
+    main()
